3c5ee0792d981468d9c65fdc8f03b963e0aa70cd
TechSynth <ismaboudg@gmail.com>
2026-02-09 10:19:28 +0100

    Implemento movimiento aleatorio del objetivo con coordenadas, se reutiliza del Ej5

--- PracticaUD4/app_sala4.js
+++ PracticaUD4/app_sala4.js
@@ -13,10 +13,46 @@ document.addEventListener("DOMContentLoaded", function () {
     let codigo = document.getElementById("codigo");
     let mensaje = document.getElementById("mensaje");
 
-    textoInicial.innerText = `¡Haz clic en ${asesino} para atraparlo!`;
-
     let img = document.createElement("img");
     img.src = `personajes/${asesino}.gif`;
     img.alt = asesino;
     objetivo.appendChild(img);
+
+    let velocidad = 300;
+    let intervalo;
+
+    function moverAleatoriamente() {
+        let maxX = zona.clientWidth - objetivo.offsetWidth;
+        let maxY = zona.clientHeight - objetivo.offsetHeight;
+        objetivo.style.left = Math.random() * maxX + "px";
+        objetivo.style.top = Math.random() * maxY + "px";
+    }
+
+    zona.addEventListener("mouseout", () => {
+        info.textContent = "Saliste del área. ¡Vuelve para continuar!";
+    });
+
+    zona.addEventListener("blur", () => {
+        info.textContent = "Saliste del área. ¡Vuelve para continuar!";
+    });
+
+    zona.addEventListener("mouseover", () => {
+    info.textContent = `¡Coge a ${asesino} antes de que escape!`;
+});
+
+    intervalo = setInterval(moverAleatoriamente, velocidad);
+
+    setTimeout(() => {
+        clearInterval(intervalo);
+        info.textContent = `¡Parece que ${asesino} se ha cansado! ¡APROVECHA!`;
+    }, 5000);
+
+    zona.addEventListener("mousemove", (e) => {
+        let rect = zona.getBoundingClientRect();
+        let x = Math.floor(e.clientX - rect.left);
+        let y = Math.floor(e.clientY - rect.top);
+        mensaje.textContent = `Tu posición: (${x}, ${y})`;
+    });
+
+    moverAleatoriamente();
 });
