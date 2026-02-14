cd6dccefd972fa021efa14505dd472b159fad8be
Casa <shawnboudg@gmail.com>
2026-02-14 16:17:59 +0100

    Práctica Unidad 5. Sala 5. Delegación de eventos, se imprime por consola si has seleccionado la prenda del armario y su tamaño.

--- PracticaUD5/app.js
+++ PracticaUD5/app.js
@@ -2,3 +2,22 @@
 Nueva sala para la práctica de la unidad 5.
 Consiste en vestir a un moñeco */
 
+
+document.addEventListener("DOMContentLoaded", () => {
+    const armario = document.querySelector('.armario');
+
+    // Uso de delegación de eventos
+    armario.addEventListener('click', (e) => {
+        // Se comprueba si la imagen seleccionada es de prenda
+        if (e.target.classList.contains('prenda')) {
+            // Para saber cual es su tamaño
+            const ancho = e.target.offsetWidth;
+            const alto = e.target.offsetHeight;
+
+            console.log(`Seleccionada prenda: ${e.target.id}. Tamaño: ${ancho}x${alto}px`);
+        }
+        else {
+            console.log("No has seleccionado nada del armario!");
+        }
+    });
+});
