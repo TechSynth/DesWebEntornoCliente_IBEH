38703e61e97074bdd7834b25ddb19bbbb0a40b29
Casa <shawnboudg@gmail.com>
2026-02-14 18:38:04 +0100

    Práctica Unidad 5. Sala 5. Manejo de propagación. Validación alfanumerica en input con key y regex.test

--- PracticaUD5/app.js
+++ PracticaUD5/app.js
@@ -6,7 +6,8 @@ Consiste en vestir a un moñeco */
 document.addEventListener("DOMContentLoaded", () => {
     const armario = document.querySelector('.armario');
     const prendas = document.querySelectorAll('.prenda');
-    const juego = document.querySelector('.juego');
+    const inputNombre = document.getElementById('nombrePersonaje');
+
     let prendaSeleccionada = null;
 
     // Uso de delegación de eventos
@@ -14,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // Se comprueba si la imagen seleccionada es de prenda
         if (e.target.classList.contains('prenda')) {
             // Guardo id, ancho y alto de la prenda seleccionada
-            prendaSeleccionada={ // Objeto con la información de la prenda seleccionada
+            prendaSeleccionada = { // Objeto con la información de la prenda seleccionada
                 id: e.target.id,
                 ancho: e.target.offsetWidth,
                 alto: e.target.offsetHeight
@@ -26,9 +27,14 @@ document.addEventListener("DOMContentLoaded", () => {
             console.log("No has seleccionado nada del armario!");
         }
     });
-    
+
+    // Evento click en body para propagación
+    document.body.addEventListener("click", () => {
+        console.log("Click en el body");
+    });
+
     prendas.forEach(prenda => {
-        prenda.onmousedown = function(event) {
+        prenda.onmousedown = function (event) {
 
             // Validacion de teclado, solo podrá arrastrar con CTRL presionado
             if (!event.ctrlKey) {
@@ -49,21 +55,46 @@ document.addEventListener("DOMContentLoaded", () => {
             function onMouseMove(event) {
                 moveAt(event.pageX, event.pageY);
             }
-            
+
             // Arrastrar y soltar
             document.addEventListener("mousemove", onMouseMove); // Mueve la prenda con el ratón
 
-            prenda.onmouseup = function(event) { // Una vez se suelte el ratón se elimina el movimiento y se deja la prenda en la posición final
+            prenda.onmouseup = function (event) { // Una vez se suelte el ratón se elimina el movimiento y se deja la prenda en la posición final
                 document.removeEventListener("mousemove", onMouseMove);
                 prenda.onmouseup = null;
-                
+
                 prenda.style.left = event.pageX - prenda.offsetWidth / 2 + "px";
                 prenda.style.top = event.pageY - prenda.offsetHeight / 2 + "px";
+                
+                // Para propagacion, si se hace click en la prenda, no se propaga al body
+                prenda.addEventListener("click", (e) => {
+                    e.stopPropagation(); // Evita que el click se propague al body
+                    console.log(`Click en prenda colocada ${prenda.id} - propagación detenida`);
+                });
             };
+            
         };
 
-        prenda.ondragstart = function() { // Para que no se duplique la prenda al arrastrar
+        prenda.ondragstart = function () { // Para que no se duplique la prenda al arrastrar
             return false;
         };
     });
+
+    // Validación de carácteres alfanuméricos 
+    inputNombre.addEventListener("keypress", (e) => {
+        const caracter = e.key; // Que tecla se ha apretado
+
+        // Valida que solo sea letra (a-z, A-Z) o número (0-9)
+        const esAlfanumerico = /^[a-zA-Z0-9]$/.test(caracter); // Devuelve true si es alfanum
+
+        if (esAlfanumerico) {
+            console.log(`Has introducido el caracter "${caracter}"`);
+            
+        }
+        else {
+            e.preventDefault(); // Evita que se escriba el caracter
+            console.log("Solo letras y números");
+        }
+    });
+    
 });
